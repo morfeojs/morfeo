56d086267b7615e7a11711d096bafc6c466781dc
Andrea Simone Porceddu <andrea@vlkstudio.com>
2021-11-05 23:23:52 +0100

    feat: preset-default improvements

--- packages/preset-default/src/base/lineHeights.ts
+++ packages/preset-default/src/base/lineHeights.ts
@@ -1,6 +1,7 @@
 import { LineHeights } from '@morfeo/spec';
 
 export const lineHeights: LineHeights = {
-  body: '1.3em',
-  heading: '1.6em',
+  body: '1.7',
+  heading: '1.5',
+  none: '1',
 };
--- packages/preset-default/src/base/spacings.ts
+++ packages/preset-default/src/base/spacings.ts
@@ -1,12 +1,20 @@
-import { Spacings } from '@morfeo/spec';
-
-export const spacings: Spacings = {
-  '2xs': '8px',
-  xs: '16px',
-  s: '24px',
-  m: '32px',
-  l: '40px',
-  xl: '48px',
-  '2xl': '56px',
-  none: '0px',
+export const spacings = {
+  none: '0rem',
+  '3xs': '.25rem',
+  '2xs': '.5rem',
+  xs: '1rem',
+  s: '1.5rem',
+  m: '2rem',
+  l: '2.5rem',
+  xl: '3rem',
+  '2xl': '3.5rem',
+  '3xl': '4rem',
+  '4xl': '5rem',
+  '5xl': '6rem',
 };
+
+type LocalSpacings = typeof spacings;
+
+declare module '@morfeo/spec' {
+  export interface Spacings extends LocalSpacings {}
+}
