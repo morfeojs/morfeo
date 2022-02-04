24168abd89acde56d457e66b3b9d9ecf9e27e568
mauroerta <mauro.erta@gmail.com>
2022-02-04 21:56:45 +0100

    fix: commented unused code

--- scripts/bootstrap-app.ts
+++ scripts/bootstrap-app.ts
@@ -1,53 +1,53 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 import { spawnSync } from 'child_process';
-import { filterInternalDependencies } from './utils';
+// import { filterInternalDependencies } from './utils';
 
 const args = process.argv.slice(2);
 const [APP_NAME] = args;
 
-function getPackageJsonWithoutInternalDependencies(appName: string) {
-  const packageJsonFile = fs.readFileSync(path.join(appName, 'package.json'), {
-    encoding: 'utf-8',
-  });
-  const packageJson = JSON.parse(packageJsonFile);
+// function getPackageJsonWithoutInternalDependencies(appName: string) {
+//   const packageJsonFile = fs.readFileSync(path.join(appName, 'package.json'), {
+//     encoding: 'utf-8',
+//   });
+//   const packageJson = JSON.parse(packageJsonFile);
 
-  const { dependencies, peerDependencies, devDependencies } = packageJson;
-  const newDependencies = filterInternalDependencies(dependencies);
-  const newPeerDependencies = filterInternalDependencies(peerDependencies);
-  const newDevDependencies = filterInternalDependencies(devDependencies);
+//   const { dependencies, peerDependencies, devDependencies } = packageJson;
+//   const newDependencies = filterInternalDependencies(dependencies);
+//   const newPeerDependencies = filterInternalDependencies(peerDependencies);
+//   const newDevDependencies = filterInternalDependencies(devDependencies);
 
-  return {
-    ...packageJson,
-    dependencies: newDependencies,
-    peerDependencies: newPeerDependencies,
-    devDependencies: newDevDependencies,
-  };
-}
+//   return {
+//     ...packageJson,
+//     dependencies: newDependencies,
+//     peerDependencies: newPeerDependencies,
+//     devDependencies: newDevDependencies,
+//   };
+// }
 
-function removeInternalPackagesFromPackageJson(appName: string) {
-  const packageJsonPath = path.join(appName, 'package.json');
-  const packageJsonFile = fs.readFileSync(packageJsonPath, {
-    encoding: 'utf-8',
-  });
-  const tempPackageJsonPath = path.join(appName, 'package.tmp.json');
-  const newPackageJson = getPackageJsonWithoutInternalDependencies(appName);
-  fs.writeFileSync(tempPackageJsonPath, packageJsonFile);
-  fs.writeFileSync(
-    packageJsonPath,
-    JSON.stringify(newPackageJson, undefined, 2),
-  );
-}
+// function removeInternalPackagesFromPackageJson(appName: string) {
+//   const packageJsonPath = path.join(appName, 'package.json');
+//   const packageJsonFile = fs.readFileSync(packageJsonPath, {
+//     encoding: 'utf-8',
+//   });
+//   const tempPackageJsonPath = path.join(appName, 'package.tmp.json');
+//   const newPackageJson = getPackageJsonWithoutInternalDependencies(appName);
+//   fs.writeFileSync(tempPackageJsonPath, packageJsonFile);
+//   fs.writeFileSync(
+//     packageJsonPath,
+//     JSON.stringify(newPackageJson, undefined, 2),
+//   );
+// }
 
-function restorePackageJson(appName: string) {
-  const packageJsonPath = path.join(appName, 'package.json');
-  const tempPackageJsonPath = path.join(appName, 'package.tmp.json');
-  const packageJsonFile = fs.readFileSync(tempPackageJsonPath, {
-    encoding: 'utf-8',
-  });
-  fs.writeFileSync(packageJsonPath, packageJsonFile);
-  fs.unlinkSync(tempPackageJsonPath);
-}
+// function restorePackageJson(appName: string) {
+//   const packageJsonPath = path.join(appName, 'package.json');
+//   const tempPackageJsonPath = path.join(appName, 'package.tmp.json');
+//   const packageJsonFile = fs.readFileSync(tempPackageJsonPath, {
+//     encoding: 'utf-8',
+//   });
+//   fs.writeFileSync(packageJsonPath, packageJsonFile);
+//   fs.unlinkSync(tempPackageJsonPath);
+// }
 
 function shellCommand(command: string, params: string[] = [], options = {}) {
   spawnSync(command, params, {
