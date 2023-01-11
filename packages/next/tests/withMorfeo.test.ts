ce8823cc1a49cbfd080f851791e30c65c2a17bf9
Mauro Erta <mauro.erta@sap.com>
2023-01-11 23:01:22 +0100

    test: added tests to @morfeo/next package

---
+++ packages/next/tests/withMorfeo.test.ts
@@ -0,0 +1,47 @@
+import { MorfeoWebpackPlugin } from '@morfeo/webpack';
+import { withMorfeo } from '../src';
+
+describe('withMorfeo', () => {
+  it('should inject the morfeo webpack plugin to the passed configuration', () => {
+    const webpack = jest.fn();
+
+    const config = withMorfeo({ webpack });
+    const webpackConfig = { plugins: [] };
+
+    // @ts-ignore
+    config.webpack(webpackConfig, {});
+
+    expect(webpackConfig.plugins[0]).toEqual(
+      new MorfeoWebpackPlugin({
+        babel: {
+          presets: ['next/babel'],
+        },
+      }),
+    );
+  });
+
+  it('should inject the additional babel configuration if passed', () => {
+    const webpack = jest.fn();
+
+    const config = withMorfeo(
+      { webpack },
+      {
+        babel: {
+          presets: ['fake/preset'],
+        },
+      },
+    );
+    const webpackConfig = { plugins: [] };
+
+    // @ts-ignore
+    config.webpack(webpackConfig, {});
+
+    expect(webpackConfig.plugins[0]).toEqual(
+      new MorfeoWebpackPlugin({
+        babel: {
+          presets: ['fake/preset', 'next/babel'],
+        },
+      }),
+    );
+  });
+});
