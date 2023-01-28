c2ccb30e59638121ab5bffe2e069938289f58da0
Mauro Erta <mauro.erta@sap.com>
2023-01-28 16:51:08 +0100

    fix: typo in createUseClasses test

--- packages/css/tests/createUseClasses.test.ts
+++ packages/css/tests/createUseClasses.test.ts
@@ -1,7 +1,7 @@
 import { createUseClasses } from '../src';
 
 describe('createUseClasses', () => {
-  it('should throw an exception in case `css` is executed', () => {
+  it('should throw an exception in case `createUseClasses` is executed', () => {
     expect(() => createUseClasses({})).toThrow();
   });
 });
