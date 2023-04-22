9d62bf72920714b21aa75fa9ec47b20a1f1ad402
Mauro Erta <mauro.erta@sap.com>
2023-04-22 10:44:06 +0200

    fix(typescript): preserveSymlinks

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,13 +1,9 @@
 import { createUnplugin } from 'unplugin';
 import { getMorfeoUnpluginOptions } from './plugin';
-import type { PluginOption } from 'vite';
-import type { MorfeoPluginOptions } from './types';
 
 const unplugin = createUnplugin(getMorfeoUnpluginOptions);
 
-type VitePlugin = (options: MorfeoPluginOptions) => PluginOption;
-
-export const MorfeoVitePlugin = unplugin.vite as VitePlugin;
+export const MorfeoVitePlugin = unplugin.vite;
 export const MorfeoRollupPlugin = unplugin.rollup;
 export const MorfeoWebpackPlugin = unplugin.webpack;
 export const MorfeoRspackPlugin = unplugin.rspack;
