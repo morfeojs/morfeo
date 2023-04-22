f9aa5e3acd0e0f8925836e221f00b9f3e706e51b
Mauro Erta <mauro.erta@sap.com>
2023-04-22 14:50:03 +0200

    chore(web-extension): fixed manifest versioning script

--- scripts/version-web-extension.ts
+++ scripts/version-web-extension.ts
@@ -23,30 +23,7 @@ const webExtensionManifest = JSON.parse(
   fs.readFileSync(WEB_EXTENSION_MANIFEST_PATH, { encoding: 'utf8' }),
 );
 
-const currentVersion = webExtensionManifest.version as string;
-
-let [major = 0, minor = 0, patch = 0] = currentVersion
-  .split('.')
-  .map((number: string) => {
-    const parsed = Number.parseInt(number);
-    return Number.isNaN(parsed) ? 0 : parsed;
-  });
-
-if (VERSION_TYPE === 'major') {
-  major += 1;
-  minor = 0;
-  patch = 0;
-} else if (VERSION_TYPE === 'minor') {
-  minor += 1;
-  patch = 0;
-} else {
-  patch += 1;
-}
-
-const newVersion = [major, minor, patch].join('.');
-
-webExtensionPackageJson.version = newVersion;
-webExtensionManifest.version = newVersion;
+webExtensionManifest.version = webExtensionPackageJson.version;
 
 fs.writeFileSync(
   WEB_EXTENSION_PACKAGE_JSON_PATH,
