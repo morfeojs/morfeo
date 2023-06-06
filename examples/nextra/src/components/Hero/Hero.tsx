0224c3b81e377531a2a2c64a6846c947ce6da239
Luca Cacciarru <86778250+lucacacciarru@users.noreply.github.com>
2023-06-06 07:28:56 +0200

    feat(docs): Added lottie animations to the FeatureCards (#781)

--- examples/nextra/src/components/Hero/Hero.tsx
+++ examples/nextra/src/components/Hero/Hero.tsx
@@ -10,7 +10,10 @@ const classes = morfeo.css({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    py: 'raw:10rem',
+    py: {
+      default: '5xl',
+      sm: 'raw:10rem',
+    },
     gradient:
       'raw:radial-gradient(circle at 40% 40%, #445ac74d 0%, transparent 40%),radial-gradient(circle at 60% 60%, rgba(183, 65, 14, 0.4) 0%, transparent 30%)',
   },
@@ -18,7 +21,10 @@ const classes = morfeo.css({
     componentName: 'Typography',
     variant: 'display',
     color: 'gray.lightest',
-    fontSize: '4xl',
+    fontSize: {
+      default: '3xl',
+      sm: '4xl',
+    },
     lineHeight: 'none',
     mb: 's',
   },
@@ -26,12 +32,36 @@ const classes = morfeo.css({
     componentName: 'Typography',
     variant: 'h2',
     color: 'gray.light',
-    maxWidth: 'raw:800px',
+    fontWeight: 'regular',
+    fontSize: {
+      default: 'l',
+      sm: 'xl',
+    },
+    maxWidth: {
+      default: '100',
+      sm: 'raw:800px',
+    },
   },
   cta: {
     display: 'flex',
     py: 'm',
-    gap: 'm',
+    gap: {
+      default: 's',
+      sm: 'm',
+    },
+    width: '100',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: {
+      default: 'column',
+      sm: 'row',
+    },
+    '& a': {
+      minW: {
+        default: '100',
+        sm: 'none',
+      },
+    },
   },
 });
 
@@ -41,9 +71,7 @@ export function Hero() {
   return (
     <section className={classes.container}>
       <h1 className={classes.title}>
-        <TypingAnimation words={words} /> Without
-        <br />
-        compromises.
+        <TypingAnimation words={words} /> <br /> Without compromises.
       </h1>
       <h2 className={classes.subtitle}>
         Morfeo is a build-time CSS-in-JS solution for the next level theming,
