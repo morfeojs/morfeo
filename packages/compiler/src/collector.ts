04f0fdea12cd7d8ce2f6192d7079418816acb4a8
Mauro Erta <mauro.erta@sap.com>
2023-09-15 08:23:43 +0200

    chore: fix deepscan issue

--- packages/compiler/src/collector.ts
+++ packages/compiler/src/collector.ts
@@ -50,13 +50,14 @@ function createCollector() {
           new Set([
             ...DEFAULT_BABEL_PRESETS,
             ...(options.babel?.presets || []),
-            ...(pluginOptions.babel?.presets || []),
+            ...(pluginOptions?.babel?.presets || []),
           ]),
         ),
         plugins: Array.from(
           new Set([
             ...DEFAULT_BABEL_PLUGINS,
-            ...(pluginOptions.babel?.plugins || []),
+            ...(options.babel?.plugins || []),
+            ...(pluginOptions?.babel?.plugins || []),
             [morfeoBabelPlugin, {}],
           ]),
         ),
